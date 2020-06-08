88a0796dd59b886fed9cafc54ae5ed9d157e0fe8
atlas <doroshuk33@yandex.by>
2020-06-08 20:38:10 +0300

    daa

--- person/atl-wp-person-team.php
+++ person/atl-wp-person-team.php
@@ -20,4 +20,19 @@ require_once __DIR__.'/classes/CreateMetabox.php';
             {
                 new CreateMetabox($option);
             }
+            
+/*Create template for custom post type*/
+add_filter( 'template_include', 'include_template_function', 1 );
+function include_template_function( $template_path ) {
+    if ( get_post_type() == 'teamperson' ) {
+        if ( is_single() ) {
+            if ( $theme_file = locate_template( array ( 'single-teamperson.php' ) ) ) {
+                $template_path = $theme_file;
+            } else {
+                $template_path = plugin_dir_path( __FILE__ ) . 'templates/single-teamperson.php';
+            }
+        }
+    }
+    return $template_path;
+}
 ?>
